8e790f3522c678a2230d535360f6305492fcbdca
Juan Manuel Torres <juan.manuel.torres@mi.unc.edu.ar>
2026-08-28 19:32:23 -0300

    feat: add Atlas Shiny app entrypoint

---
+++ app.R
@@ -0,0 +1,12 @@
+source("R/filter_projects.R", local = TRUE)
+source("R/read_processed_data.R", local = TRUE)
+source("R/build_atlas_app.R", local = TRUE)
+source("R/run_atlas_app.R", local = TRUE)
+
+atlas_data_dir <- Sys.getenv(
+  "ATLAS_DATA_DIR",
+  unset = file.path("data", "processed")
+)
+
+app <- build_atlas_app_from_processed(atlas_data_dir)
+app
